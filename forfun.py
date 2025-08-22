3520728e6d0697ceefe6ae9691c1c7031478cbd2
Heloisa-Santos <heloisa84551050@gmail.com>
2025-08-21 21:45:08 -0300

    first commit

---
+++ forfun.py
@@ -0,0 +1,40 @@
+import cohere
+import flask 
+from dotenv import load_dotenv
+import os
+
+from flask_cors import CORS
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+co = cohere.ClientV2("Sqp4Wv2PEpmq7H1yJAkmDqhham3cTFlT1DmDYir5")
+
+
+def chat_with_cohere(prompt, message):
+    
+    response = co.chat(
+        model="command-light",
+        messages= [{"role":"system", "content": "Você é um assistente amigável e prestativo."}]+message,
+        max_tokens=100,
+        temperature=0.5
+    )
+    return response.message.content[0].text
+    
+
+if __name__ == "__main__":
+    message =[]
+    app = flask.Flask(__name__)
+    CORS(app)
+    @app.route("/chat", methods=["POST"])
+    def chat():
+        data = flask.request.json
+        user_message = data.get("message")
+        message.append({"role": "user", "content": user_message})
+        if not user_message:
+            return flask.jsonify({"error": "No message provided"}), 400
+        
+        response_text = chat_with_cohere(user_message, message)
+        return flask.jsonify({"response": response_text})
+
+    app.run(debug=True)
